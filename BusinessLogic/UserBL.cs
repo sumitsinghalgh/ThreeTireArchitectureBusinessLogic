b04bc65e7cc7159c70fdd79ce604e97379c1855a
sumitsinghalgh <sumsgl198630pd@gmail.com>
2021-09-24 23:08:33 +0530

    My Second Commitment

---
+++ BusinessLogic/UserBL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObj;
+using DataAccess;
+
+namespace BusinessLogic
+{
+    public class UserBL
+    {
+        public CustomBO AddUser(UserBO objuserBO)
+        {
+            return new UserDAL().Adduser(objuserBO);
+        }
+    }
+}
